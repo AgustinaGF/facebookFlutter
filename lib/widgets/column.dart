b27ea01e12e041db01406bbf7fc2e663c52a4cb8
AgustinaGF <agustinagomez385@gmail.com>
2023-06-22 12:52:38 -0300

    column part3

--- lib/widgets/column.dart
+++ lib/widgets/column.dart
@@ -16,37 +16,17 @@ class MyColumn extends StatelessWidget {
           child: Container(
             color: Colors.grey,
             child: Column(
+              //de esta manera va a posicionar los container en cada extremo de la pagina
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                ///cuando se utiliza un column dentro de otro column su altura se calcula de forma dinamica en base a la altura de los hijos
-                const Column(
-                  children: [
-                    Text('Hello'),
-                    Text(
-                      'World',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+                Container(
+                  height: 100,
+                  color: Colors.pinkAccent,
                 ),
-
-                ///este elemento sirve para agregar un padding entre Columnas
                 // const SizedBox(height: 20),
-                Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      color: Colors.pinkAccent,
-                    ),
-                    // const SizedBox(height: 20),
-                    Container(
-                      height: 100,
-                      color: Colors.blue,
-                    ),
-                  ],
+                Container(
+                  height: 100,
+                  color: Colors.blue,
                 ),
               ],
             ),
