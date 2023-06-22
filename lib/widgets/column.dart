eaef604f8995fe40d36e92760fadb98f848ef637
AgustinaGF <agustinagomez385@gmail.com>
2023-06-22 12:49:13 -0300

    column part2

--- lib/widgets/column.dart
+++ lib/widgets/column.dart
@@ -6,34 +6,53 @@ class MyColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
-        style: const TextStyle(color: Colors.black),
-        child: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.symmetric(
-              vertical: 55,
-            ),
-            child: Center(
-              child: Container(
-                  color: Colors.grey,
-                  child: Column(
+      style: const TextStyle(color: Colors.black),
+      child: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(
+          vertical: 55,
+        ),
+        child: Center(
+          child: Container(
+            color: Colors.grey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ///cuando se utiliza un column dentro de otro column su altura se calcula de forma dinamica en base a la altura de los hijos
+                const Column(
+                  children: [
+                    Text('Hello'),
+                    Text(
+                      'World',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
 
-                      ///con este elemento se logra que el column ocupe el espacio respecto a sus hijos y no que busque ocupar todo el height
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Text('Hello'),
-                        const Text(
-                          'World',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Container(
-                          height: 100,
-                          color: Colors.pinkAccent,
-                        )
-                      ])),
-            )));
+                ///este elemento sirve para agregar un padding entre Columnas
+                // const SizedBox(height: 20),
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      color: Colors.pinkAccent,
+                    ),
+                    // const SizedBox(height: 20),
+                    Container(
+                      height: 100,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
