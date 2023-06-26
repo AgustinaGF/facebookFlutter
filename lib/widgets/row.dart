22e960f05e3e3df2a53405cfedcae7b026b9d198
AgustinaGF <agustinagomez385@gmail.com>
2023-06-26 19:36:06 -0300

    row2

--- lib/widgets/row.dart
+++ lib/widgets/row.dart
@@ -8,12 +8,9 @@ class MyRow extends StatelessWidget{
     return Container(
       color: Colors.white,
       // el widget Row nos permite posicionar los elementos uno al lado del otro
-      child: Center(child:SizedBox(
-        height: 200,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //alinea los elementos al inicio del elemento mas alto
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children:[
             Container(
               width: 50,
@@ -32,8 +29,6 @@ class MyRow extends StatelessWidget{
           )
           ],
           ),
-      ),
-    ),
-    );
+      );
   }
 }
