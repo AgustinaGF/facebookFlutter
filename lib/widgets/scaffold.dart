8b055690ea431c2880ecfd0677eb818a4670c628
AgustinaGF <agustinagomez385@gmail.com>
2023-06-27 18:00:36 -0300

    drawer

--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -5,7 +5,20 @@ import 'package:flutter/services.dart';
 class MyScaffold extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
+
+    return Scaffold(
+
+      endDrawer: const Drawer(
+        elevation: 0,
+        child: Column(
+          children:  [
+            SizedBox(height: 40,),
+            Text("test@test.com", style: TextStyle(color: Colors.black))
+          ],
+        ),
+        
+      ),
+      appBar: AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(
         // este funciona tanto para ios como android
         statusBarBrightness: Brightness.dark
@@ -13,7 +26,7 @@ class MyScaffold extends StatelessWidget {
     backgroundColor: const Color(0xff01579b),
     elevation: 10,
     shadowColor: Colors.red,
-    titleTextStyle: const TextStyle(color: Colors.white, fontSize: 18,),title:const Row(mainAxisSize: MainAxisSize.min,children: [Text('Flutter App')]),)
+    titleTextStyle: const TextStyle(color: Colors.white, fontSize: 18),title:const Row(mainAxisAlignment: MainAxisAlignment.center,mainAxisSize: MainAxisSize.min,children: [Text('Flutter App'), ]),)
     );
   }
 }
