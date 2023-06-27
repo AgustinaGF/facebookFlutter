29650232e89e54f7431fce15c691527623698005
AgustinaGF <agustinagomez385@gmail.com>
2023-06-27 12:58:05 -0300

    stack1

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class MyStack extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
- return Container(
+ return DefaultTextStyle(style: const TextStyle(color: Colors.white), child: 
+  Container(
   color: Colors.white,
   child: Stack(
     alignment: Alignment.center,
     children: [
       Container(width: 150,height: 150,color: Colors.green,),
-       Container(width: 20,height: 20,color: Colors.red,)
+       Container(width: 20,height: 20,color: Colors.red,),
+       const Text('Hola'),
     ],
   )
- );
-  }
+ )
+  );}
 
 }
