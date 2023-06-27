3f7ff330295d401ab38a7ccb7a03463dd2064710
AgustinaGF <agustinagomez385@gmail.com>
2023-06-27 13:04:56 -0300

    positioned

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -6,13 +6,21 @@ class MyStack extends StatelessWidget{
  return DefaultTextStyle(style: const TextStyle(color: Colors.white), child: 
   Container(
   color: Colors.white,
-  child: Stack(
-    alignment: Alignment.center,
-    children: [
-      Container(width: 150,height: 150,color: Colors.green,),
-       Container(width: 20,height: 20,color: Colors.red,),
-       const Text('Hola'),
-    ],
+  child: Center(
+    child: Container(
+      color: Colors.grey,
+      width: 400,
+      height: 500,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(top:0, left:0,child: Container(width: 150,height: 150,color: Colors.green,),)
+          ,
+           Container(width: 50,height: 50,color: Colors.red,),
+           const Text('Hola'),
+        ],
+      ),
+    ),
   )
  )
   );}
