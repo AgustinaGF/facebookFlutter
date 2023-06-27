770e6465b3cfd6439046a23bb4eb1e13209b4a9f
AgustinaGF <agustinagomez385@gmail.com>
2023-06-27 16:08:43 -0300

    elevation

--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -2,15 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class MyScaffold extends StatelessWidget {  @override
+class MyScaffold extends StatelessWidget { 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(
         // este funciona tanto para ios como android
         statusBarBrightness: Brightness.dark
       ),
-    backgroundColor: Colors.grey,
-    titleTextStyle: const TextStyle(color: Colors.black, fontSize: 18,),title:const Text('Flutter App'),)
+    backgroundColor: const Color(0xff01579b),
+    elevation: 10,
+    titleTextStyle: const TextStyle(color: Colors.white, fontSize: 18,),title:const Row(mainAxisSize: MainAxisSize.min,children: [Text('Flutter App')]),)
     );
   }
 }
