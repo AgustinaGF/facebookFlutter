4c407acf111394007d7b9ff1450c42cf33e0fc3e
AgustinaGF <agustinagomez385@gmail.com>
2023-06-27 13:15:05 -0300

    Positioned.fill

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -14,9 +14,8 @@ class MyStack extends StatelessWidget{
       child: Stack(
         alignment: Alignment.center,
         children: [
-          Positioned(
-            //si asigno left y right lo que va a hacer es posicionar el elemento dejando espacio tanto a la izquierda como a la derecha
-            top:0, left:0, right:0,bottom: 0,child: Container(height: 150,color: Colors.green,child: const Text('mundo'),),)
+          Positioned.fill(
+            child: Container(height: 150,color: Colors.green,child: const Text('mundo'),),)
           ,
            Container(width: 50,height: 50,color: Colors.red,),
            const Text('Hola'),
