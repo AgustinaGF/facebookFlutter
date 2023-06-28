c9cbbdbe8a2142b3a7f04a0bf28bab54c6bf394e
AgustinaGF <agustinagomez385@gmail.com>
2023-06-28 16:54:29 -0300

    scroll with safeArea

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -5,15 +5,27 @@ class MySingleChildScrollView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         //la altura a va a estar dada por el componente padre, en este caso el Scaffold
-        body: SingleChildScrollView(
+        body: SafeArea(
+      bottom: false,
       child: Column(
-        children: List.generate(
-          18,
-          (index) => Container(
-            color: Colors.primaries[index],
-            height: 100,
+        children: [
+          Text('Hello'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: List.generate(
+                  18,
+                  (index) => Container(
+                    color: Colors.primaries[index],
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: Text('$index'),
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     ));
   }
