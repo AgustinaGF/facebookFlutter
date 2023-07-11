2c895fae304850bd041cc9f64c3e8b1d0296f715
AgustinaGF <agustinagomez385@gmail.com>
2023-07-11 16:06:09 -0300

    add image

--- lib/facebook_ui/facebook_ui.dart
+++ lib/facebook_ui/facebook_ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:widgest/facebook_ui/widgets/avatar.dart';
 import '../icons/custom_icon_icons.dart';
 import 'widgets/circle_button.dart';
 
@@ -8,32 +9,38 @@ class FacebookUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      systemOverlayStyle: SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
-      ),
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      leadingWidth: 150,
-      leading: SvgPicture.asset(
-        'assets/logos/facebook.svg',
-        colorFilter: const ColorFilter.mode(Colors.blueAccent, BlendMode.srcIn),
-        width: 150,
-      ),
-      actions: [
-        CircleButton(color: Color(0xffBFBFBF), iconData: CustomIcons.search),
-        SizedBox(width: 15),
-        CircleButton(color: Color(0xffFE7574), iconData: CustomIcons.bell),
-        SizedBox(width: 15),
-        CircleButton(
-          color: Color(0xff7BBAFF),
-          iconData: CustomIcons.user_friends,
-          showBadge: true,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.light,
         ),
-        SizedBox(width: 15),
-        CircleButton(color: Color(0xff1C86E4), iconData: CustomIcons.messenger),
-        SizedBox(width: 15),
-      ],
-    ));
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 150,
+        leading: SvgPicture.asset(
+          'assets/logos/facebook.svg',
+          colorFilter:
+              const ColorFilter.mode(Colors.blueAccent, BlendMode.srcIn),
+          width: 150,
+        ),
+        actions: [
+          CircleButton(color: Color(0xffBFBFBF), iconData: CustomIcons.search),
+          SizedBox(width: 15),
+          CircleButton(color: Color(0xffFE7574), iconData: CustomIcons.bell),
+          SizedBox(width: 15),
+          CircleButton(
+            color: Color(0xff7BBAFF),
+            iconData: CustomIcons.user_friends,
+            showBadge: true,
+          ),
+          SizedBox(width: 15),
+          CircleButton(
+              color: Color(0xff1C86E4), iconData: CustomIcons.messenger),
+          SizedBox(width: 15),
+        ],
+      ),
+      body: ListView(
+        children: [Avatar(size: 100, asset: 'assets/users/1.jpg')],
+      ),
+    );
   }
 }
