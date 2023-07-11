e28b378b5caec002aaa421f0e5300b9e15d8d8f3
AgustinaGF <agustinagomez385@gmail.com>
2023-07-11 12:50:31 -0300

    appBar

--- lib/facebook_ui/facebook_ui.dart
+++ lib/facebook_ui/facebook_ui.dart
@@ -1,9 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../icons/custom_icon_icons.dart';
 
 class FacebookUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+      ),
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leadingWidth: 150,
+      leading: SvgPicture.asset(
+        'assets/logos/facebook.svg',
+        colorFilter: const ColorFilter.mode(Colors.blueAccent, BlendMode.srcIn),
+        width: 150,
+      ),
+      actions: [
+        Container(
+          width: 40,
+          height: 40,
+          decoration:
+              const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+          child: const Icon(
+            CustomIcons.search,
+            color: Colors.white,
+            size: 16,
+          ),
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration:
+              const BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+          child: const Icon(
+            CustomIcons.bell,
+            color: Colors.white,
+            size: 16,
+          ),
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration: const BoxDecoration(
+              color: Colors.lightBlue, shape: BoxShape.circle),
+          child: const Icon(
+            CustomIcons.user_friends,
+            color: Colors.white,
+            size: 16,
+          ),
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration: const BoxDecoration(
+              color: Colors.blueAccent, shape: BoxShape.circle),
+          child: const Icon(
+            CustomIcons.messenger,
+            color: Colors.white,
+            size: 16,
+          ),
+        )
+      ],
+    ));
   }
 }
--- lib/icons/custom_icon_icons.dart
+++ lib/icons/custom_icon_icons.dart
@@ -11,8 +11,10 @@
 ///      fonts:
 ///       - asset: fonts/CustomIcons.ttf
 ///
+/// 
 ///
-///
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/widgets.dart';
 
 class CustomIcons {
@@ -21,24 +23,14 @@ class CustomIcons {
   static const _kFontFam = 'CustomIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData like =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData settings =
-      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home =
-      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData edit =
-      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData messenger =
-      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData bell =
-      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData search =
-      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData photos =
-      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData video_camera =
-      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData user_friends =
-      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData like = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData settings = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData edit = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData messenger = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bell = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData photos = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData video_camera = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user_friends = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
